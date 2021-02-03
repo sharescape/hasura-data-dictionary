8a21e6d3fe644b9359ed8d6dd75289bd263ee621
Christian Nieves <christian@nieves.io>
2021-02-03 16:23:42 -0700

    add bash entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+env_missing() { echo "missing $* environment variable"; exit 1; }
+
+if [ -n "$HASURA_GRAPHQL_SERVER_HOST" ]; then
+  ENVSTR="NEXT_PUBLIC_HASURA_URL=$HASURA_GRAPHQL_SERVER_HOST"
+else
+  env_missing "HASURA_GRAPHQL_SERVER_HOST"
+fi
+
+if [ -n "$HASURA_GRAPHQL_ADMIN_SECRET" ]; then
+  ENVSTR="$ENVSTR\nNEXT_PUBLIC_HASURA_ADMIN_SECRET=$HASURA_GRAPHQL_ADMIN_SECRET"
+else
+  env_missing "HASURA_GRAPHQL_ADMIN_SECRET"
+fi
+
+if [ -n "$DATA_DICTIONARY_PORT" ]; then
+  ENVSTR="$ENVSTR\nNEXT_PUBLIC_REACT_APP_URL=http://localhost:$DATA_DICTIONARY_PORT"
+else
+  ENVSTR="$ENVSTR\nNEXT_PUBLIC_REACT_APP_URL=http://localhost:3000"
+fi
+
+
+cat > next.config.js <<- JAVASCRIPT
+module.exports = {
+  basePath: '$DATA_DICTIONARY_BASE_PATH',
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+  }
+}
+JAVASCRIPT
+
+printf "$ENVSTR" > .env
+
+yarn dev
